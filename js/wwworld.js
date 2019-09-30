10e8a49567f86a42757f3d54aa71926cbdb7d083
Sophia Wu <swu18@mywhitecliffe.com>
2019-09-30 21:52:06 +1300

    Updates

--- js/wwworld.js
+++ js/wwworld.js
@@ -5,3 +5,9 @@
 	scrollConverter.activate();
 
 }());
+function toggle_visibility(ww-title) {
+	 var e = document.getElementById(ww-title);
+	 if(e.style.display == 'block')
+			e.style.display = 'none';
+	 else
+			e.style.display = 'block';
