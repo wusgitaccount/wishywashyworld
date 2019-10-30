bea2c088495b534ff324f9174e5647969a33fca3
Sophia Wu <swu18@mywhitecliffe.com>
2019-10-31 12:23:32 +1300

    Manually changing horizontal scroll

--- js/wwworld.js
+++ js/wwworld.js
@@ -1,8 +1,9 @@
-//alert('hello!');
 
-$("#page-wrap").wrapInner("<table cellspacing='0'><tr>");
-$(".ww-page").wrap("<td></td>");
+// Done manually now
+// $("#page-wrap").wrapInner("<table cellspacing='0'><tr>");
+// $(".ww-page").wrap("<td></td>");
+
 $("body").mousewheel(function(event, delta) {
-		this.scrollLeft -= delta * 30;
-		event.preventDefault();
-			});
+	this.scrollLeft -= delta * 30;
+	event.preventDefault();
+});
